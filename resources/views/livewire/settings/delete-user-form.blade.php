49d9347b6d7698e1dba1360372bc84d7ed833102
Tead <teadmarvin@gmail.com>
2026-02-22 00:49:30 +0800

    refactor: remove unnecessary modal trigger wrapper from delete button
    Simplify the delete account button markup by directly attaching the modal
    open event handler to the button element, eliminating the redundant
    modal.trigger wrapper.

--- resources/views/livewire/settings/delete-user-form.blade.php
+++ resources/views/livewire/settings/delete-user-form.blade.php
@@ -4,11 +4,9 @@
         <flux:subheading>{{ __('Delete your account and all of its resources') }}</flux:subheading>
     </div>
 
-    <flux:modal.trigger name="confirm-user-deletion">
-        <flux:button variant="danger" x-data="" x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')">
-            {{ __('Delete account') }}
-        </flux:button>
-    </flux:modal.trigger>
+    <flux:button variant="danger" x-data="" x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')">
+        {{ __('Delete account') }}
+    </flux:button>
 
     <flux:modal name="confirm-user-deletion" :show="$errors->isNotEmpty()" focusable class="max-w-lg">
         <form method="POST" wire:submit="deleteUser" class="space-y-6">
